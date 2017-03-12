3fd8194b85d7eab3bfa9518238be58d41c427546
ningbingjian <ningbingjian@126.com>
2017-03-12 22:13:02 +0800

    new

--- minispark/src/main/scala/com/ning/spark/SparkConf.scala
+++ minispark/src/main/scala/com/ning/spark/SparkConf.scala
@@ -1,8 +1,31 @@
 package com.ning.spark
 
+import java.util.concurrent.ConcurrentHashMap
+
+import com.ning.network.IOMode
+
 /**
   * Created by zhaoshufen on 2017/3/12.
   */
-object SparkConf {
+class SparkConf {
+  private val settings = new ConcurrentHashMap[String, String]()
+  def set(key: String,value: String): SparkConf = {
+    if(key == null){
+      throw new RuntimeException("null key")
+    }
+    if(value == null){
+      throw new RuntimeException("null value")
+    }
+    settings.put(key,value)
+    this
+  }
+  def get(key: String): String = {
+    return getOption(key).getOrElse(throw new NoSuchElementException(key))
+  }
+  def getOption(key: String): Option[String] = {
+    return Option(settings.get(key))
+  }
+  def ioMode(): IOMode = {
 
+  }
 }
